c59c3ed1a4560741ea8ff9889537b5534b43ab9a
Sherif Khedr <shrief.issawy@gmail.com>
2022-01-11 18:06:17 +0200

    backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const items = require('./routes/api/items')
+
+const app = express()
+
+//bodyparser Middleware
+app.use(bodyParser.json()) // or also app.use(express.json())
+
+//DB config
+const db = require('./config/keys').mongoURI
+
+//connect to mongo
+mongoose.connect(db)
+    .then(() => console.log("MongoDB connected..."))
+    .catch(err => console.log(err))
+
+//use routes
+app.use('/api/items', items)
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
