fd9eb1a4fdddb13233f21de7d3690b3b64112f4c
Sherif Khedr <shrief.issawy@gmail.com>
2022-01-11 21:16:03 +0200

    setting up front end

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
+
